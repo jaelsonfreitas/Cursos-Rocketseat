d9c7a35b50b1a92a9c620d73d5bbceff6965f331
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-15 20:53:10 -0300

    comentários em js.

---
+++ Discover/trilha_Fundamentar/Guia-estelar-Java-Script/comentarios.js
@@ -0,0 +1,4 @@
+// aqui eu tenho um comentário em linha
+
+/* O código abaixo escreve uma mensagem no devtools
+console.log('Bem vindos aos Starter'); sendo assim tenho um comentário multi linha*/git 
