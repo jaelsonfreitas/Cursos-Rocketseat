8b8d8ce9b35c64133af5f481cd173d7043116318
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-15 22:38:43 -0300

    falando sobre Object.

---
+++ Discover/trilha_Fundamentar/Guia-estelar-Java-Script/object.js
@@ -0,0 +1,15 @@
+/*
+Object
+propriedades/ atributos
+funcionalidades
+
+escrevo { propriedade:  "Valor"}
+*/
+
+console.log({
+    name: "Jaelson"
+    idade: 36,
+    andar: function (){
+        console.log('andar');
+    }
+});
