db5b74929bb8779eb97f6a2deab03246eddc2b8c
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-01-15 22:26:01 -0300

    falando sobre scripts.

---
+++ Discover/trilha_Fundamentar/Guia-estelar-Java-Script/string.js
@@ -0,0 +1,12 @@
+/*
+    String
+ É uma cadeia de caracteres  
+ ex: ABCDE - sendo assim formamos textos
+ para isso usamos 
+ '' Aspas simples
+ "" Aspas Duplas 
+ ou
+ `` template literals ou template strings - permitem fazer multi linhas, como tambem permitem colocar expressões (interpolações).
+*/
+
+console.log('Jaélson');
